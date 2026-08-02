78514f9c79bbd31a7610d2f02734e513dff964ca
bipu990 <bipu0000000@gmail.com>
2026-08-02 23:19:34 +0600

    Programming Contest Spring-26 [Preliminary - A Slot .....( if / else )

---
+++ 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+    int a,b;
+    scanf ("%d %d",&a,&b);
+    
+    if (a<b) printf ("yayy");
+    else if (a=b) printf ("perfect");
+    else printf ("booo");
+    
+    return 0;
+}
